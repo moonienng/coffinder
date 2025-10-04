544d2d1b6a82b2551eb7035a50a841a0ebf65080
gabriella <128649836+moonienng@users.noreply.github.com>
2025-10-04 13:56:18 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -69,3 +69,48 @@ const imgUrl = cafe.photos?.[0]?.photo_reference
 wrapper.appendChild(card);
 container.appendChild(wrapper);
   
+const hammertime = new Hammer(wrapper);
+  hammertime.on('swipeleft', () => {
+    wrapper.style.transform = 'translateX(-150%) rotate(-15deg)';
+    wrapper.style.opacity = 0;
+    setTimeout(() => wrapper.remove(), 100);
+  });
+  hammertime.on('swiperight', () => {
+    saveCafe(JSON.stringify(cafeData));
+    wrapper.style.transform = 'translateX(150%) rotate(15deg)';
+    wrapper.style.opacity = 0;
+    setTimeout(() => wrapper.remove(), 100);
+  });
+  function saveCafe(cafeJSON) {
+  const cafe = JSON.parse(cafeJSON);
+  let saved = JSON.parse(localStorage.getItem('savedCafes') || '[]');
+
+  if (!saved.find(c => c.place_id === cafe.place_id)) {
+  saved.push(cafe);
+  localStorage.setItem('savedCafes', JSON.stringify(saved));
+  alert(`${cafe.name} saved!`);
+}
+    else {
+    alert(`${cafe.name} is already saved.`);
+  }
+}
+  function showSaved() {
+  const container = document.querySelector('.cards');
+  container.innerHTML = '';
+  const saved = JSON.parse(localStorage.getItem('savedCafes') || '[]');
+  if (saved.length === 0) {
+  container.innerHTML = '<p>No saved cafes yet 😢</p>';
+  return;
+}
+saved.forEach(cafe => {
+  const card = document.createElement('div');
+  card.className = 'location-card';
+  card.innerHTML = `
+    <img src="${cafe.photo}" alt="${cafe.name}" />
+    <h3>${cafe.name}</h3>
+    <p>⭐️ Rating: ${cafe.rating}</p>
+      `;
+  container.appendChild(card);
+    });
+    
+    
